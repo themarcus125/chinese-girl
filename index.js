bc6d28e0004a157a8f652f9a80136be92e831a2e
Khoa Ngo <ngohakhoa@gmail.com>
2021-07-29 13:32:22 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function playSong() {
+  var audio = document.querySelector('audio');
+  audio.play();
+}
+
+function onButtonClick() {
+  alert('WHAT ARE YOU TRYING TO DO ??? GET BACK AND HAPPY BIRTHDAY!!!');
+}
